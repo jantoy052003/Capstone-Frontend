a0b70f3eb0122c856103239ac958b36e6f8e9aeb
Jan Michael Caasi <janmichaelcaasi@gmail.com>
2024-06-15 06:16:16 +0800

    changed baseURL to render

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  // baseURL: 'http://localhost:8001/api',
+  baseURL: 'https://capstone-laravel-api.onrender.com',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
