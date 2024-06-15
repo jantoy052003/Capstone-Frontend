ab66a3f33659603a69a9a0e3604e50d8c433b078
Jan Michael Caasi <janmichaelcaasi@gmail.com>
2024-06-15 10:35:30 +0800

    testing access-control baseURL

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: 'http://localhost:8001/api',
-  baseURL: 'https://capstone-laravel-api.onrender.com',
+  baseURL: 'postgres://capstone_laravel_api_t9ez_user:JCDpwDHDbyEqyDjnMwNu6plhIB20Y3I1@dpg-cpm48mdds78s738tj8t0-a/capstone_laravel_api_t9ez',
   headers: {
     // 'Accept': 'application/json',
     // 'Content-Type': 'application/json'
