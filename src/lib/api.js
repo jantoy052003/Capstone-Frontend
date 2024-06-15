1188364b3e56c5224de1e8f686d9fe821ed23cf9
Jan Michael Caasi <janmichaelcaasi@gmail.com>
2024-06-15 10:26:00 +0800

    testing access-control

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,8 +4,11 @@ const api = axios.create({
   // baseURL: 'http://localhost:8001/api',
   baseURL: 'https://capstone-laravel-api.onrender.com',
   headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
+    // 'Accept': 'application/json',
+    // 'Content-Type': 'application/json'
+    'Access-Control-Allow-Origin':  'http://127.0.0.1:3000',
+    'Access-Control-Allow-Methods': 'POST',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization'
   }
 })
 
