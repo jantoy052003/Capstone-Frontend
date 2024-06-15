9781a33e91c8081b53f67826234c94a1c379c92e
Jan Michael Caasi <janmichaelcaasi@gmail.com>
2024-06-15 19:46:08 +0800

    added .env for api

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 
-const api = axios.create({
+const http = axios.create({
   // baseURL: 'http://localhost:8001/api',
-  baseURL: 'postgres://capstone_laravel_api_t9ez_user:JCDpwDHDbyEqyDjnMwNu6plhIB20Y3I1@dpg-cpm48mdds78s738tj8t0-a/capstone_laravel_api_t9ez',
+  baseURL: import.meta.env.VITE_API,
+  //baseURL: 'https://capstone-laravel-api.onrender.com',
   headers: {
-    // 'Accept': 'application/json',
-    // 'Content-Type': 'application/json'
-    'Access-Control-Allow-Origin':  'postgres://capstone_laravel_api_t9ez_user:JCDpwDHDbyEqyDjnMwNu6plhIB20Y3I1@dpg-cpm48mdds78s738tj8t0-a/capstone_laravel_api_t9ez',
-    'Access-Control-Allow-Methods': 'POST',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+    // 'Access-Control-Allow-Origin':  'postgres://capstone_laravel_api_t9ez_user:JCDpwDHDbyEqyDjnMwNu6plhIB20Y3I1@dpg-cpm48mdds78s738tj8t0-a/capstone_laravel_api_t9ez',
+    // 'Access-Control-Allow-Methods': 'POST',
+    // 'Access-Control-Allow-Headers': 'Content-Type, Authorization'
   }
 })
 
-export default api
+export default http
